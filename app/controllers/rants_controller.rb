e9819b827b9c4e082f70e6993481205c03d0b9de
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-05-10 18:13:20 +0200

    filled CRUD portions

--- app/controllers/rants_controller.rb
+++ app/controllers/rants_controller.rb
@@ -12,15 +12,39 @@ class RantsController < ApplicationController
     @aunty = Aunty.find(params[:aunty_id])
     @rant = current_user.rants.build(rant_params)
     if @rant.save
-      binding.pry
-      redirect rant_path(@rant)
+      advice = @aunty.advices.build(rant_id: @rant.id)
+      # advice.feedback = @aunty.note.sample.message
+      advice.feedback = "Reflect on your life choices, boo."
+      advice.save
+      redirect_to advice_path(advice)
     else
       render :new
   end
 end
 
   def show
+    @aunty = Aunty.find(params[:aunty_id])
     @rant = Rant.find(params[:id])
+  end
+
+
+  def edit
+    @aunty = Aunty.find(params[:aunty_id])
+    @rant = Rant.find(params[:id])
+  end
+
+  def update
+    @aunty = Aunty.find(params[:aunty_id])
+    @rant = Rant.find(params[:id])
+    if @rant.update(rant_params)
+      redirect_to advice_path(advice)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @rant.destroy
   end
   private
   def rant_params
