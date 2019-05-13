a8d74e45292d9a55b22f694054261ca651cc6c06
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-05-13 19:58:40 +0200

    add update method

--- app/controllers/advices_controller.rb
+++ app/controllers/advices_controller.rb
@@ -2,4 +2,17 @@ class AdvicesController < ApplicationController
   def show
     @advice = Advice.find(params[:id])
   end
+  def update
+    @advice= Advice.find(params[:id])
+    if @advice.update(advice_params)
+      redirect_to rants_path
+    else
+      render :edit
+    end
+  end
+
+ private
+  def advice_params
+    params.require(:advice).permit(:feedback,:helpful)
+  end
 end
