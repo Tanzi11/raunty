85609123b820f3f77b4257ba65c2508b0b39ee74
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-05-13 14:12:43 +0200

    add omniauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
   resources :aunties do
     resources :rants, only: [:new, :create]
   end
   resources :advices, only: [:show, :update]
+  resources :rants, except: [:new, :create]
+  resources :users, only: [:show]
 end
