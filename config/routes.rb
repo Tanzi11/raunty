fcfe4ee992a5692e2e1c51a55efa87dd3a4b4a48
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-05-15 17:19:07 +0200

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "aunties#index"
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
   resources :aunties do
     resources :rants, only: [:new, :create]
